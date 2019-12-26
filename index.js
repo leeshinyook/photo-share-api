28be439deb0853fc1e39ceb14a3061e7858b9d8f
shinyooklee <tlsdbr97@gmail.com>
2019-12-27 00:50:00 +0900

    Add : typeDefs 객체도 반환하는 방법

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
 // 1. 'apolloServer 불러오기.
-const {
-  ApolloServer
-} = require('apollo-server');
+const { ApolloServer } = require("apollo-server");
 
 const typeDefs = `
+    type Photo {
+      id: ID!
+      url: String!
+      name: String!
+      description: String
+    }
     type Query {
         totalPhotos: Int!
+        allPhotos: [Photo!]!
     }
-`
+    type Mutation {
+        postPhoto(name: String! description: String): Boolean!
+    }
+`;
+
+let photos = [];
 
 const resolvers = {
   Query: {
-    totalPhotos: () => 42
+    totalPhotos: () => photos.length
+  },
+  Mutation: {
+    postPhoto(parent, args) {
+      photos.push(args);
+      return true;
+    }
   }
-}
+};
 // 2. 서버 인스턴스를 새로 만든다.
 // 3. typeDefs(스키마)와 리졸버를 객체에 넣어 전달한다.
 const server = new ApolloServer({
   typeDefs,
   resolvers
-})
+});
 
 // 4. 웹서버 구동을 위해 listen메서드를 호출한다.
-server.listen().then(({
-  url
-}) => console.log("GraphQl server running on ${url}"));
+server
+  .listen()
+  .then(({ url }) => console.log("GraphQl server running on ${url}"));
