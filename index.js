33d57239b4aaf4dc8f04ee12e10dae880c3e71d3
leeshinyook <tlsdbr97@gmail.com>
2019-12-28 22:18:07 +0900

    Set : apollo-server-express와 express로 서버구축

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 // 1. 'apolloServer 불러오기.
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer } = require("apollo-server-express");
+const express = require("express");
+const { GraphQLScalarType } = require("graphql");
 
+// 2. express()를 호출하여 익스프레스 애플리케이션을 만든다.
+var app = express();
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// 3. applyMiddleware()를 호출하여 미들웨어가 같은 경로에 마운트되도록 합니다.
+server.applyMiddleware({ app });
+
+// 4. 홈 라우트를 만든다.
+app.get("/", (req, res) => {
+  res.end("PhotoShare API에 오신 것을 환영합니다.");
+});
+app.listen({ port: 4000 }, () => {
+  console.log(
+    `GraphQl Server running @ http://localhost:4000${server.graphqlPath}`
+  );
+});
 const typeDefs = `
+    scalar DateTime
     enum PhotoCategory {
       SELFIE
       PORTRAIT
@@ -15,6 +34,7 @@ const typeDefs = `
       name: String!
       description: String
       category: PhotoCategory!
+      created: DateTime
       postedBy: User!
       taggedUsers: [User!]!
     }
@@ -32,7 +52,7 @@ const typeDefs = `
     }
     type Query {
         totalPhotos: Int!
-        allPhotos: [Photo!]!
+        allPhotos(after: DateTime): [Photo!]!
     }
     type Mutation {
         postPhoto(input: PostPhotoInput!): Photo!
@@ -41,6 +61,11 @@ const typeDefs = `
 
 let _id = 0;
 // let photos = [];
+let d = new Date("12/28/2019");
+console.log(d.toISOString());
+const serialize = value => new Date(value).toISOString();
+const parseValue = value => new Date(value);
+
 let users = [
   {
     githubLogin: "mHattrup",
@@ -61,20 +86,23 @@ let photos = [
     name: "Dropping the Heart Chute",
     description: "The heart chute is one of my favorite chutes",
     category: "ACTION",
-    githubLogin: "gPlake"
+    githubLogin: "gPlake",
+    created: "3-28-2017"
   },
   {
     id: "2",
     name: "Enjoying the sunshine",
     category: "SELFIE",
-    githubLogin: "sSchmidt"
+    githubLogin: "sSchmidt",
+    created: "3-28-2007"
   },
   {
     id: "3",
     name: "Gunbarrel 25",
     description: "25 laps on gunbarrel today",
     category: "LANDSCAPE",
-    githubLogin: "sSchmidt"
+    githubLogin: "sSchmidt",
+    created: "3-28-2005"
   }
 ];
 let tags = [
@@ -87,7 +115,9 @@ let tags = [
 const resolvers = {
   Query: {
     totalPhotos: () => photos.length,
-    allPhotos: () => photos
+    allPhotos: (parent, args) => {
+      args.after;
+    }
   },
   Mutation: {
     postPhoto(parent, args) {
@@ -119,16 +149,12 @@ const resolvers = {
         .filter(tag => tag.userID === parent.id)
         .map(tag => tag.photoID)
         .map(photoID => photos.find(p => p.id === photoID))
-  }
+  },
+  DateTime: new GraphQLScalarType({
+    name: "DateTime",
+    description: "A Valid date time value",
+    parseValue: value => new Date(value),
+    serialize: value => new Date(value).toISOString,
+    parseLiteral: ast => ast.value
+  })
 };
-// 2. 서버 인스턴스를 새로 만든다.
-// 3. typeDefs(스키마)와 리졸버를 객체에 넣어 전달한다.
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
-
-// 4. 웹서버 구동을 위해 listen메서드를 호출한다.
-server
-  .listen()
-  .then(({ url }) => console.log("GraphQl server running on ${url}"));
